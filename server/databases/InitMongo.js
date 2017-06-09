dd164af3fe73ed9c9065254197df30a58c55cf0f
Eric Martin <ericmartin827@gmail.com>
2017-06-09 17:54:28 -0500

    databases/InitMongo.js: initial addition

---
+++ server/databases/InitMongo.js
@@ -0,0 +1,23 @@
+/**
+ * The InitMongo.js file is a configuration file used to create and set the
+ * the properties of MongoDB Collections. It should be loaded and excecuted in
+ * a server environement prior to deployment to ensure errors such as dupicate
+ * database entries are not generated for the UAH Library. This ensures
+ * that there will never be multiple records describing the same entity such
+ * as a student, a falculty member, a play, etc..
+ *
+ * @author Eric William Martin
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
