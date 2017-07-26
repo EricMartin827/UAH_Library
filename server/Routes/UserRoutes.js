729f5e976d288d00ef96173e819fb5b023d494b6
Eric Martin <ericmartin827@gmail.com>
2017-07-26 18:54:14 -0500

    UserApp.js moved into UserRoutes.js

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -54,6 +54,52 @@ async function addMultipleUsers(data) {
     return data;
 }
 
+/******************************************************************************/
+/*********************** User Login/Logout/Me Routes **************************/
+/******************************************************************************/
+
+userApp.patch("/login", (req, res) => {
+
+    var user = req.body;
+    if (!(user.email && user.password)) {
+	return res.status(400).send(
+	    makeErrno(ECINVAL,
+		      `User Failed To Provide Their Email And Password`));
+    }
+
+    User.findByCredentials(user.email, user.password)
+	.then((user) => {
+	    var token;
+	    if (token = user.getRegisterToken()) {
+		res.header("x-register", token).send(user);
+	    } else {
+		user.initAuthToken("user").then((token) => {
+		    res.header("x-user", token).send(user);
+		}).catch((err) => {
+		    res.status(400).send(err);
+		})
+	    }
+	})
+	.catch((err) => {
+	    res.status(401).send(err);
+	});
+});
+
+userApp.patch("/logout", authUser, (req, res) => {
+
+    var user = req.header["x-user"];
+    user.clearToken("user").then(() => {
+	res.send(user);
+    }).catch((err) => {
+	res.status(400).send(err);
+    });
+});
+
+userApp.get("/me", authUser, (req, res) => {
+
+    var user = req.header["x-user"];
+    res.send(user);
+});
 
 /******************************************************************************/
 /************************* Get Routes For Users *******************************/
