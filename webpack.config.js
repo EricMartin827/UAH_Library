14c68c8564f0aa87ed5032daa37b498cd84a4445
Eric Martin <ericmartin827@gmail.com>
2017-09-02 14:13:18 -0400

    Webpack, React: initial setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+
+    /* Set the entry point for the client-side application */
+    entry : "./src/index.js",
+
+    /* Name the output "object" JS file as bundle.js and place
+     * it in the build directory. The path.join() function
+     * appends the build directory to the absolute path __dirname.
+     */
+    output : {
+	path : path.join(__dirname, "build"),
+	filename : "bundle.js"
+    },
+
+    module : {
+
+	rules : [
+	    {
+		use : "babel-loader",
+		test : /\.jsx?$/,
+		exclude : /node_modules/
+	    }
+	]
+    }
+}
+
