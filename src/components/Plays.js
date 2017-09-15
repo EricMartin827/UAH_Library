ae7e4c82e8b6e91080e6bf8f482ed092278d9ee6
Li <CSding6@gmail.com>
2017-09-14 20:56:59 -0400

    refactered plays page with pagination table

--- src/components/Plays.js
+++ src/components/Plays.js
@@ -1,52 +1,79 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
 import _ from "lodash";
-
 import { fetchPlays } from "./../actions";
+import { ButtonToolbar, Button, Pagination } from 'react-bootstrap';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class Plays extends Component {
+
+    constructor(props) {
+      super(props);
+
+      this.options = {
+        onPageChange: this.onPageChange.bind(this),
+        onSizePerPageList: this.sizePerPageListChange.bind(this)
+      };
+    }
 
     componentDidMount() {
         this.props.fetchPlays();
     }
 
-    renderPlays() {
-        return _.map(this.props.plays, play => {
-            console.log(play);
-            return (
-                <tr key={play._id}>
-                    <td>{play.title}</td>
-                    <td>{play.genre}</td>
-                    <td>{play.actorCount}</td>
-                    <td>{play.authorLast}, {play.authorFirst}</td>
-                    <td>{play.timePeriod}</td>
-                    <td>{play.costumeCount}</td>
-                    <td>{play.hasSpectacle + ""}</td>
-                    <td>{play.copies}</td>
-                </tr>
-            );
-        });
+    sizePerPageListChange(sizePerPage) {
+    //   alert(`sizePerPage: ${sizePerPage}`);
+    }
+
+    onPageChange(page, sizePerPage) {
+    //   alert(`page: ${page}, sizePerPage: ${sizePerPage}`);
+    }
+
+    renderPlaysTable() {
+        const plays = _.map(this.props.plays);
+        const selectRowProp = {
+          mode: 'checkbox',
+          clickToSelect: true
+        };
+
+        return (
+            <BootstrapTable data={plays} pagination={ true } options={ this.options } selectRow={ selectRowProp }>
+                <TableHeaderColumn width='150' dataField="title" isKey={true} dataSort={true}>
+                    Title
+                </TableHeaderColumn>
+                <TableHeaderColumn width='150' dataField="genre">
+                    Genre
+                </TableHeaderColumn>
+                <TableHeaderColumn  width='150' dataField="actorCount">
+                    Actor Count
+                </TableHeaderColumn>
+                <TableHeaderColumn width='150' dataField="authorLast">
+                    Author
+                </TableHeaderColumn>
+                <TableHeaderColumn width='150' dataField="timePeriod">
+                    Time Period
+                </TableHeaderColumn>
+                <TableHeaderColumn width='150' dataField="costumeCount">
+                    Costume Count
+                </TableHeaderColumn>
+                <TableHeaderColumn width='150' dataField="hasSpectacle">
+                    hasSpectacle
+                </TableHeaderColumn>
+                <TableHeaderColumn width='150' dataField="copies">
+                    copies
+                </TableHeaderColumn>
+            </BootstrapTable>
+        )
     }
 
     render() {
         return (
             <div>
-                <h3>This is the Plays page</h3>
-                <table className='list-group'>
-                    <tbody>
-                    <tr>
-                        <td>Title</td>
-                        <td>Genre</td>
-                        <td>Actor Count</td>
-                        <td>Author</td>
-                        <td>Time Period</td>
-                        <td>Costume Count</td>
-                        <td>Spectacle?</td>
-                        <td>Copies</td>
-                    </tr>
-                    {this.renderPlays()}
-                    </tbody>
-                </table>
+                <ButtonToolbar>
+                <Button bsStyle="success" bsSize="small" active>
+                    Display Detail
+                </Button>
+                </ButtonToolbar>
+                {this.renderPlaysTable()}
             </div>
         );
     }
