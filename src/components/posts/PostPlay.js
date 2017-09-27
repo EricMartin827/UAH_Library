1c7d843108f53a17a1620da382d52d6a0376c7f8
jherndon8 <jherndon8@gatech.edu>
2017-09-26 22:25:08 -0400

    Added validation

--- src/components/posts/PostPlay.js
+++ src/components/posts/PostPlay.js
@@ -46,8 +46,35 @@ class PostPlay extends Component {
 
 
 function validate(values) { 
-    //TODO: validate input
-    return {}
+    const errors = {};
+    if (!values.title) {
+            errors.title = "Enter A Title";
+    }
+    if (!values.genre) {
+        errors.genre = "Enter A Genre";
+    }
+    if (!(values.actorCount - 0)) {
+        errors.actorCount = "Enter Number of Actors";
+    }
+    if (!values.authorLast) {
+        errors.authorLast = "Enter Author's Last Name";
+    }
+    if (!values.authorFirst) {
+        errors.authorFirst = "Enter Author's First Name";
+    }
+    if (!values.timePeriod) {
+        errors.timePeriod = "Enter time period";
+    }
+    if (!(values.costumeCount - 0)) {
+        errors.costumeCount = "Enter Number of Costumes"; 
+    }
+    if (!values.hasSpectacle) {
+        errors.hasSpectacle = "Enter spectacle name or 'false'"
+    }
+    if (!(values.copies - 0)) {
+        errors.copies = "Enter Number of Copies of Script"
+    }
+    return errors;
 }
 
 export default reduxForm({
