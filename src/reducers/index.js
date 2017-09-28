41ff1974fba7bcbae47cb9a0d3d23b8ebf03b8a2
Andrew Li <andrewli@lawn-128-61-21-139.lawn.gatech.edu>
2017-09-28 18:25:39 -0400

    navigation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,13 +6,15 @@ import { reducer as formReducer } from "redux-form";
 import currentUserReducer from "./CurrentUserReducer.js"
 import usersReducer from "./UsersReducer.js";
 import playsReducer from './reducer_plays';
+import navsReducer from './reducer_navs';
 
 
 const rootReducer = combineReducers({
     currentUser : currentUserReducer,
     users       : usersReducer,
     plays       : playsReducer,
-    form        : formReducer
+    form        : formReducer,
+    navs        : navsReducer
 });
 
 export default rootReducer;
